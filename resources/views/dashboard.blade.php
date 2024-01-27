efdf907c4a2cb47d2e6d2c4ed993f9b6d53d6160
dikaproject <dikagilang2007@gmail.com>
2024-01-27 14:22:34 +0700

    live chat update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <br>
+                    <br>
+                    <a href="{{ route('chatify') }}">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            LIVE CHAT DEMO FEATURE
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
